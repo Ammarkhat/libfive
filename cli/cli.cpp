f914713ae765140d77fddfb3d1bd0c0c0a3655da
Ammar Hattab <Ammarkhat@gmail.com>
2022-02-19 05:13:57 -0500

    read radiuses and distances from command line

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -16,6 +16,23 @@
 #include "libfive/render/discrete/heightmap.hpp"
 
 int main(int argc, char** argv){
+    int r1 = 1;
+    int r2 = 1;
+    int x1 = 1;
+    int x2 = 1;
+    if(argc > 1){
+        r1 = atoi(argv[1]);
+    }
+    if(argc > 2){
+        r2 = atoi(argv[2]);
+    }
+    if(argc > 3){
+        x1 = atoi(argv[3]);
+    }
+    if(argc > 4){
+        x2 = atoi(argv[4]);
+    }
+
     // Unlike the C bindings, the C++ interface manages memory automatically
     // through flyweight handles and RAII.  These objects will be freed
     // when they go out of scope
@@ -24,8 +41,8 @@ int main(int argc, char** argv){
     auto z = libfive::Tree::Z();
 
     // Arithemetic is overloaded for the libfive::Tree type
-    auto out = min( sqrt(((x-1) * (x-1)) + (y * y) + (z * z)) - 1,
-                    sqrt(((x+1) * (x+1)) + (y * y) + (z * z)) - 1) - 0.75;
+    auto out = min( sqrt(((x-x1) * (x-x1)) + (y * y) + (z * z)) - r1,
+                    sqrt(((x+x2) * (x+x2)) + (y * y) + (z * z)) - r2) - 0.75;
 
     // Pick the target region to render
     auto bounds = libfive::Region<3>({-4, -4, -4}, {4, 4, 4});

bbc3e03e7e03691a920079870528cfc90a82c271
Ammar Hattab <Ammarkhat@gmail.com>
2022-02-19 04:51:13 -0500

    fixed namespace for compilation

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -2,22 +2,35 @@
 
 #include "libfive.h"
 
+#include "libfive/eval/evaluator.hpp"
+#include "libfive/tree/opcode.hpp"
+#include "libfive/tree/tree.hpp"
+#include "libfive/tree/data.hpp"
+#include "libfive/eval/eval_deriv_array.hpp"
+#include "libfive/eval/eval_interval.hpp"
+#include "libfive/render/brep/region.hpp"
+#include "libfive/render/brep/contours.hpp"
+#include "libfive/render/brep/mesh.hpp"
+#include "libfive/render/brep/settings.hpp"
+#include "libfive/render/discrete/voxels.hpp"
+#include "libfive/render/discrete/heightmap.hpp"
+
 int main(int argc, char** argv){
     // Unlike the C bindings, the C++ interface manages memory automatically
     // through flyweight handles and RAII.  These objects will be freed
     // when they go out of scope
-    auto x = Kernel::Tree::X();
-    auto y = Kernel::Tree::Y();
-    auto z = Kernel::Tree::Z();
+    auto x = libfive::Tree::X();
+    auto y = libfive::Tree::Y();
+    auto z = libfive::Tree::Z();
 
-    // Arithemetic is overloaded for the Kernel::Tree type
+    // Arithemetic is overloaded for the libfive::Tree type
     auto out = (x * x) + (y * y) + (z * z) - 1;
 
     // Pick the target region to render
-    auto bounds = Region<3>({-2, -2, -2}, {2, 2, 2});
+    auto bounds = libfive::Region<3>({-2, -2, -2}, {2, 2, 2});
 
     // Mesh::render returns a unique_ptr, so it cleans up automatically
-    Mesh::render(out, bounds)->saveSTL("sphere.stl");
+    libfive::Mesh::render(out, bounds)->saveSTL("sphere.stl");
 
     return 0;
 }
